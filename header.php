b75083c141d352f1af28d9b109750132cec53a47
hgcosta <hugocosta.devs@gmail.com>
2023-07-21 11:20:49 -0300

    feature: modulos 01, 02, 03, 04

--- header.php
+++ header.php
@@ -18,7 +18,8 @@
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/owl.carousel.min.js"></script>
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/bootstrap.bundle.js"></script>
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/main.js"></script>
-        <title><?php bloginfo('name')?> | <?php the_title()?></title>
+        <link href="hugodev.com.br/portfolios/pixelart.ttf" rel="stylesheet">
+        <title><?php bloginfo('name')?></title>
         <?php wp_head();?>
     </head>
     <body <?php body_class()?> >
--- page-home.php
+++ page-home.php
@@ -1,31 +1,103 @@
 <?php // Template Name: Home ?> 
 <?php get_header()?>
-<section class="" id="">
+
+<section class="hero" id="">
    
 </section>
+<section class="modulo-01">
+    <h2>Vamos conhecer </br> os nossos maiores inimigos desse desafio?</h2>
+    <div class="container">
+        <div class="content-modulo-01">
+            <div class="col-img">
+                <!-- <img class="bg" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-mobile.png" alt="">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-da-desculpa.png" alt=""> -->
+                <picture>
+                    <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-mobile.png">
+                    <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-da-desculpa.png" alt="Rei da Desculpa">
+                </picture>  
+            </div>
+            <div class="col-info">
+                <h4>Boss 01</h4>
+                <h5>Rei das desculpas!</h5>
+                <h3>MÓDULO 01: COMECE AQUI - 03/08/2023</h3>
+                <ul class="list-untyled">
+                    <li>Aula <strong>M01.01</strong>: Boas vindas + Introdução ao novo MFEA <strong>2.0</strong> </li>
+                    <li>Aula <strong>M01.02</strong>: Engatinhar, andar, correr, pular!!! Respeite seu tempo e pare de olhar a grama do vizinho!!</li>
+                    <li>Aula <strong>M01.03</strong>: Hora de se preparar para a jornada! Monte seu Plano de negócios</li>
+                    <li>Aula <strong>M01.04</strong>: Zoom out, ter um escritório de Arquitetura é muito mais que fazer arquitetura!! (Arquitetura x disciplinas x gestão)</li>
+                    <li>Aula <strong>M01.05</strong>: Produtividade... produza! (Planejando a semana, bigwin, trabalhando em HomeOffice, Coworking ou Escritório físico, procrastinação)</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="modulo-02">
+    <div class="content-modulo">
+        <div class="col-info">
+            <h4>Boss 02</h4>
+            <h5>Ogro da bagunça</h5>
+            <h3>MÓDULO 02: ARRUMANDO A CASA – 10/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M02.01</strong>: Introduzindo a Gestão!!! Áreas Operacionais e Gerencias</li>
+                <li>Aula <strong>M02.02</strong>: Arrumando sua Casa - Não espere ser roubado para trocar a fechadura!</li>
+                <li>Aula <strong>M02.03</strong>: Mapeie os processos e crie procedimentos (Entrada / Fornecedores / Processos / Saída / Cliente)</li>
+                <li>Aula <strong>M02.04</strong>: EAP – Estrutura Analítica de Projeto</li>
+                <li>Aula <strong>M02.05</strong>: MEN - Mínimo Esforço Necessário</li>
+            </ul>
+        </div>
+        <div class="col-img">
+            <picture>
+                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-mobile.png">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-completo.png" alt="Ogro da bagunça">
+            </picture>  
+                <!-- <img class="bg" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-mobile.png" alt="">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-completo.png" alt=""> -->
+        </div>
+    </div>
+</section>
+<section class="modulo-03">
+    <div class="content-modulo">
+        <div class="col-info">
+            <h4>Boss 03</h4>
+            <h5>Princesa da Incerteza</h5>
+            <h3>MÓDULO 03: CAPTANDO, CONVERTENDO E PRECIFICANDO - 17/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M03.01</strong>: Básico para Captação de clientes On e Offline + Funil de vendas </li>
+                <li>Aula <strong>M03.02</strong>: BRANDING (Leticia Milani) + Material de Apoio</li>
+                <li>Aula <strong>M03.03</strong>: Básico para Conversão das propspecções geradas! Como converter</li>
+                <li>Aula <strong>M03.04</strong>: Básico para Precificação por HP (Não use m<strong>2</strong>)</li>
+                <li>Aula <strong>M03.05</strong>: Apresentando a proposta</li>
+            </ul>
+        </div>
+        <div class="col-img">
+            <picture>
+                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/princesa-incerteza-mobile.png">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/princesa-incerteza.png" alt="Princesa da Incerteza">
+            </picture>  
+        </div>
+    </div>
+</section>
+<section class="modulo-04">
+    <div class="content-modulo">
+        <div class="col-img">
+            <picture>
+                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo-mobile.png">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo.png" alt="SR. Prejuízo">
+            </picture>  
+        </div>
+        <div class="col-info">
+            <h4>Boss 04</h4>
+            <h5>SR. Prejuízo</h5>
+            <h3>MÓDULO 04: PILARES DE GESTÃO – 24/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M04.01</strong>: Básico para <strong>4</strong> pilares de gestão: Custos</li>
+                <li>Aula <strong>M04.02</strong>: Básico para <strong>4</strong> pilares de gestão: Prazos</li>
+                <li>Aula <strong>M04.03</strong>: Básico para <strong>4</strong> pilares de gestão: Pessoas</li>
+                <li>Aula <strong>M04.04</strong>: Básico para <strong>4</strong> pilares de gestão: Processos</li>
+            </ul>
+        </div>
+    </div>
+</section>
 
-<!-- <script>
-        $('.owl-carousel').owlCarousel({
-        loop:true,
-        autoplay:true,
-        margin:10,
-        responsiveClass:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:1,
-            },
-            700:{
-                items:2,
-            },
-            1000:{
-                items:3,
-            }
-         }
-        })
-    </script> -->
 
 <?php get_footer()?>
