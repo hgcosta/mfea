c0cc36fc4af9b883bc923b7e5bc1247b854c9688
hgcosta <hugocosta.devs@gmail.com>
2023-07-27 07:45:38 -0300

    feature: modificação das sessoes cursos e sobre

---
+++ page-home-old.php
@@ -0,0 +1,125 @@
+<?php // Template Name: Home ?> 
+<?php get_header()?>
+
+<section class="hero" id="">
+   
+</section>
+<section class="modulo-01">
+    <h2>Vamos conhecer </br> os nossos maiores inimigos desse desafio?</h2>
+    <div class="container">
+        <div class="content-modulo-01">
+            <div class="col-img">
+                <!-- <img class="bg" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-mobile.png" alt="">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-da-desculpa.png" alt=""> -->
+                <picture>
+                    <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-mobile.png">
+                    <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-da-desculpa.png" alt="Rei da Desculpa">
+                </picture>  
+            </div>
+            <div class="col-info">
+                <h4>Boss 01</h4>
+                <h5>Rei das desculpas!</h5>
+                <h3>MÓDULO 01: COMECE AQUI - 03/08/2023</h3>
+                <ul class="list-untyled">
+                    <li>Aula <strong>M01.01</strong>: Boas vindas + Introdução ao novo MFEA <strong>2.0</strong> </li>
+                    <li>Aula <strong>M01.02</strong>: Engatinhar, andar, correr, pular!!! Respeite seu tempo e pare de olhar a grama do vizinho!!</li>
+                    <li>Aula <strong>M01.03</strong>: Hora de se preparar para a jornada! Monte seu Plano de negócios</li>
+                    <li>Aula <strong>M01.04</strong>: Zoom out, ter um escritório de Arquitetura é muito mais que fazer arquitetura!! (Arquitetura x disciplinas x gestão)</li>
+                    <li>Aula <strong>M01.05</strong>: Produtividade... produza! (Planejando a semana, bigwin, trabalhando em HomeOffice, Coworking ou Escritório físico, procrastinação)</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="modulo-02">
+    <div class="content-modulo">
+        <div class="col-info">
+            <h4>Boss 02</h4>
+            <h5>Ogro da bagunça</h5>
+            <h3>MÓDULO 02: ARRUMANDO A CASA – 10/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M02.01</strong>: Introduzindo a Gestão!!! Áreas Operacionais e Gerencias</li>
+                <li>Aula <strong>M02.02</strong>: Arrumando sua Casa - Não espere ser roubado para trocar a fechadura!</li>
+                <li>Aula <strong>M02.03</strong>: Mapeie os processos e crie procedimentos (Entrada / Fornecedores / Processos / Saída / Cliente)</li>
+                <li>Aula <strong>M02.04</strong>: EAP – Estrutura Analítica de Projeto</li>
+                <li>Aula <strong>M02.05</strong>: MEN - Mínimo Esforço Necessário</li>
+            </ul>
+        </div>
+        <div class="col-img">
+            <picture>
+                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-mobile.png">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-completo.png" alt="Ogro da bagunça">
+            </picture>  
+                <!-- <img class="bg" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-mobile.png" alt="">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-completo.png" alt=""> -->
+        </div>
+    </div>
+</section>
+<section class="modulo-03">
+    <div class="content-modulo">
+        <div class="col-info">
+            <h4>Boss 03</h4>
+            <h5>Princesa da Incerteza</h5>
+            <h3>MÓDULO 03: CAPTANDO, CONVERTENDO E PRECIFICANDO - 17/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M03.01</strong>: Básico para Captação de clientes On e Offline + Funil de vendas </li>
+                <li>Aula <strong>M03.02</strong>: BRANDING (Leticia Milani) + Material de Apoio</li>
+                <li>Aula <strong>M03.03</strong>: Básico para Conversão das propspecções geradas! Como converter</li>
+                <li>Aula <strong>M03.04</strong>: Básico para Precificação por HP (Não use m<strong>2</strong>)</li>
+                <li>Aula <strong>M03.05</strong>: Apresentando a proposta</li>
+            </ul>
+        </div>
+        <div class="col-img">
+            <picture>
+                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/princesa-incerteza-mobile.png">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/princesa-incerteza.png" alt="Princesa da Incerteza">
+            </picture>  
+        </div>
+    </div>
+</section>
+<section class="modulo-04">
+    <div class="content-modulo">
+        <div class="col-img">
+            <picture>
+                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo-mobile.png">
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo.png" alt="SR. Prejuízo">
+            </picture>  
+        </div>
+        <div class="col-info">
+            <h4>Boss 04</h4>
+            <h5>SR. Prejuízo</h5>
+            <h3>MÓDULO 04: PILARES DE GESTÃO – 24/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M04.01</strong>: Básico para <strong>4</strong> pilares de gestão: Custos</li>
+                <li>Aula <strong>M04.02</strong>: Básico para <strong>4</strong> pilares de gestão: Prazos</li>
+                <li>Aula <strong>M04.03</strong>: Básico para <strong>4</strong> pilares de gestão: Pessoas</li>
+                <li>Aula <strong>M04.04</strong>: Básico para <strong>4</strong> pilares de gestão: Processos</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<section class="modulo-05">
+    <div class="content-modulo">
+        <div class="col-img">
+            <picture>
+                <!-- <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo-mobile.png"> -->
+                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/pirraia.png" alt="SR. Prejuízo">
+            </picture>  
+        </div>
+        <div class="col-info">
+            <!-- <h4>Herói</h4> -->
+            <h5>RAIMITO</h5> 
+            <h3>MÓDULO 05: VIDA UTIL DO PROJETO – 31/08/2023</h3>
+            <ul class="list-untyled">
+                <li>Aula <strong>M05.01</strong>: Vida Útil do Projeto x Caminho Critico</li>
+                <li>Aula <strong>M05.02</strong>: Um Bom Briefing pode mudar tudo! Saiba como fazer</li>
+                <li>Aula <strong>M05.03</strong>: Cronograma: Não gaste seus dias uteis tempo a toa… Saiba como parar o tempo nos projetos!</li>
+                <li>Aula <strong>M05.04</strong>: Comunicação oficial/ formal: Ata de reunião + Documentos C.I +E-mail = Rastreabilidade </li>
+                <li>Aula <strong>M05.05</strong>: V.O: Validação do Orçamento </li>
+                <li>Aula <strong>M05.06</strong>: Vá na Obra... Mas como?</li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<?php get_footer()?>
--- page-home.php
+++ page-home.php
@@ -4,100 +4,232 @@
 <section class="hero" id="">
    
 </section>
-<section class="modulo-01">
-    <h2>Vamos conhecer </br> os nossos maiores inimigos desse desafio?</h2>
+<section class="sobre-curso">
+    <div class="container content-sobre">
+        <div class="col-info">
+            <h2>SOBRE O CURSO​</h2>
+            <p>Nosso curso de Gestão para escritórios de Arquitetura e Design de Interiores tem como objetivo ensinar aos profissionais e empreendedores da área a enxergar seu negocio como uma empresa, e criar/aplicar técnicas, procedimentos e processos que irão transformar seus projetos em linhas de produção, no quesito: Prazo, Entregas e gargalos.</p>
+            <p>Focaremos em Gestão! De Custos, Prazos, Pessoas e Processos, aprenderemos a definir Briefing e o valor da sua hora trabalhada, para precificar os projetos, falaremos sobre gestão a vista, contratos e ética na arquitetura x captação de cliente.</p>
+        </div>
+    </div>
+</section>
+<section class="aulas">
     <div class="container">
-        <div class="content-modulo-01">
-            <div class="col-img">
-                <!-- <img class="bg" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-mobile.png" alt="">
-                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-da-desculpa.png" alt=""> -->
-                <picture>
-                    <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-mobile.png">
-                    <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rei-da-desculpa.png" alt="Rei da Desculpa">
-                </picture>  
+        <h2>VAMOS CONHECER OS MÓDULOS DESSE DESAFIO?</h2>
+        <div id="accordion">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <span class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        MÓDULO 01: COMECE AQUI – 03/08/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        <ul class="list-untyled">
+                            <li>Aula <strong>M01.01</strong>: Boas vindas + Introdução ao novo MFEA <strong>2.0</strong> </li>
+                            <li>Aula <strong>M01.02</strong>: Engatinhar, andar, correr, pular!!! Respeite seu tempo e pare de olhar a grama do vizinho!!</li>
+                            <li>Aula <strong>M01.03</strong>: Hora de se preparar para a jornada! Monte seu Plano de negócios</li>
+                            <li>Aula <strong>M01.04</strong>: Zoom out, ter um escritório de Arquitetura é muito mais que fazer arquitetura!! (Arquitetura x disciplinas x gestão)</li>
+                            <li>Aula <strong>M01.05</strong>: Produtividade... produza! (Planejando a semana, bigwin, trabalhando em HomeOffice, Coworking ou Escritório físico, procrastinação)</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-info">
-                <h4>Boss 01</h4>
-                <h5>Rei das desculpas!</h5>
-                <h3>MÓDULO 01: COMECE AQUI - 03/08/2023</h3>
-                <ul class="list-untyled">
-                    <li>Aula <strong>M01.01</strong>: Boas vindas + Introdução ao novo MFEA <strong>2.0</strong> </li>
-                    <li>Aula <strong>M01.02</strong>: Engatinhar, andar, correr, pular!!! Respeite seu tempo e pare de olhar a grama do vizinho!!</li>
-                    <li>Aula <strong>M01.03</strong>: Hora de se preparar para a jornada! Monte seu Plano de negócios</li>
-                    <li>Aula <strong>M01.04</strong>: Zoom out, ter um escritório de Arquitetura é muito mais que fazer arquitetura!! (Arquitetura x disciplinas x gestão)</li>
-                    <li>Aula <strong>M01.05</strong>: Produtividade... produza! (Planejando a semana, bigwin, trabalhando em HomeOffice, Coworking ou Escritório físico, procrastinação)</li>
-                </ul>
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                    <span class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        MÓDULO 02: ARRUMANDO A CASA – 10/08/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="card-body">
+                    <ul class="list-untyled">
+                        <li>Aula <strong>M02.01</strong>: Introduzindo a Gestão!!! Áreas Operacionais e Gerencias</li>
+                        <li>Aula <strong>M02.02</strong>: Arrumando sua Casa - Não espere ser roubado para trocar a fechadura!</li>
+                        <li>Aula <strong>M02.03</strong>: Mapeie os processos e crie procedimentos (Entrada / Fornecedores / Processos / Saída / Cliente)</li>
+                        <li>Aula <strong>M02.04</strong>: EAP – Estrutura Analítica de Projeto</li>
+                        <li>Aula <strong>M02.05</strong>: MEN - Mínimo Esforço Necessário</li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                <h5 class="mb-0">
+                    <span class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        MÓDULO 03: CAPTANDO, CONVERTENDO E PRECIFICANDO - 17/08/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div class="card-body">
+                        <ul class="list-untyled">
+                            <li>Aula <strong>M03.01</strong>: Básico para Captação de clientes On e Offline + Funil de vendas </li>
+                            <li>Aula <strong>M03.02</strong>: BRANDING (Leticia Milani) + Material de Apoio</li>
+                            <li>Aula <strong>M03.03</strong>: Básico para Conversão das propspecções geradas! Como converter</li>
+                            <li>Aula <strong>M03.04</strong>: Básico para Precificação por HP (Não use m<strong>2</strong>)</li>
+                            <li>Aula <strong>M03.05</strong>: Apresentando a proposta</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingFour">
+                <h5 class="mb-0">
+                    <span class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        MÓDULO 04: : PILARES DE GESTÃO – 24/08/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                    <div class="card-body">
+                        <ul class="list-untyled">
+                            <li>Aula <strong>M04.01</strong>: Básico para <strong>4</strong> pilares de gestão: Custos</li>
+                            <li>Aula <strong>M04.02</strong>: Básico para <strong>4</strong> pilares de gestão: Prazos</li>
+                            <li>Aula <strong>M04.03</strong>: Básico para <strong>4</strong> pilares de gestão: Pessoas</li>
+                            <li>Aula <strong>M04.04</strong>: Básico para <strong>4</strong> pilares de gestão: Processos</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingFive">
+                <h5 class="mb-0">
+                    <span class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                        MÓDULO 05: : VIDA UTIL DO PROJETO – 31/08/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                    <div class="card-body">
+                        <ul class="list-untyled">
+                            <li>Aula <strong>M05.01</strong>: Vida Útil do Projeto x Caminho Critico</li>
+                            <li>Aula <strong>M05.02</strong>: Um Bom Briefing pode mudar tudo! Saiba como fazer</li>
+                            <li>Aula <strong>M05.03</strong>: Cronograma: Não gaste seus dias uteis tempo a toa… Saiba como parar o tempo nos projetos!</li>
+                            <li>Aula <strong>M05.04</strong>: Comunicação oficial/ formal: Ata de reunião + Documentos C.I +E-mail = Rastreabilidade </li>
+                            <li>Aula <strong>M05.05</strong>: V.O: Validação do Orçamento </li>
+                            <li>Aula <strong>M05.06</strong>: Vá na Obra... Mas como?</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingSix">
+                <h5 class="mb-0">
+                    <span class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                        MÓDULO EM CASO DE EMERGÊNCIA JP – 07/09/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+                    <div class="card-body">
+                        <ul class="list-untyled">
+                            <li><strong>ME.01</strong>: Dia ruim? Assista aqui </li>
+                            <li><strong>ME.02</strong>: Pensa em ter sociedade? Assista aqui!</li>
+                            <li><strong>ME.03</strong>: Cliente complicado? Abusivo? Assista aqui!</li>
+                            <li><strong>ME.04</strong>: A Equipe de obra não te respeita? Assista aqui!</li>
+                            <li><strong>ME.05</strong>:  Financeiro no vermelho? Calma!! Assista aqui!</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingSeven">
+                <h5 class="mb-0">
+                    <span class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                        MÓDULO BÔNUS - 07/09/2023
+                    </span>
+                </h5>
+                </div>
+                <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+                    <div class="card-body last">
+                        <ul class="list-untyled">
+                            <li><strong>MB.01</strong>: T.I PARA ARQUITETURA (RODOLFO)</li>
+                            <li><strong>MB.02</strong>: CONTABILIDADE - PASSOS INICIAIS (LUQUINHAS)</li>
+                            <li><strong>MB.03</strong>: RODRIGO ROSAR???</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="start">
+            <a href="" class="btn-start">
+                <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/start.png" alt="Start"> -->
+                Press Start
+            </a>
+        </div>
+    </div>
+</section>
+<section class="depoiments">
+    <div class="container content-depoiments">
+        <img class="detalhe" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/datalhe-ogro03.jpg" alt="">
+        <h2>Veja o que os nossos alunos têm a dizer!</h2>
+        <div class="owl-carousel owl-theme desktop">
+            <div class="item">
+                <article>
+                    <div class="info">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat hic cumque, facere, numquam eius ratione quisquam magni nisi eos, delectus assumenda aliquam totam fugiat consectetur impedit natus rem nostrum dolor!</p>
+                    </div>
+                    <div class="pessoa">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/hg.jpg" alt="">
+                        <h4>@hugoodev</h4>
+                    </div>
+                </article>    
+            </div>
+            <div class="item">
+                <article>
+                    <div class="info">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat hic cumque, facere, numquam eius ratione quisquam magni nisi eos, delectus assumenda aliquam totam fugiat consectetur impedit natus rem nostrum dolor!</p>
+                    </div>
+                    <div class="pessoa">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/hg.jpg" alt="">
+                        <h4>@hugoodev</h4>
+                    </div>
+                </article>    
+            </div>
+            <div class="item">
+                <article>
+                    <div class="info">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat hic cumque, facere, numquam eius ratione quisquam magni nisi eos, delectus assumenda aliquam totam fugiat consectetur impedit natus rem nostrum dolor!</p>
+                    </div>
+                    <div class="pessoa">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/hg.jpg" alt="">
+                        <h4>@hugoodev</h4>
+                    </div>
+                </article>    
             </div>
         </div>
     </div>
-</section>
-<section class="modulo-02">
-    <div class="content-modulo">
-        <div class="col-info">
-            <h4>Boss 02</h4>
-            <h5>Ogro da bagunça</h5>
-            <h3>MÓDULO 02: ARRUMANDO A CASA – 10/08/2023</h3>
-            <ul class="list-untyled">
-                <li>Aula <strong>M02.01</strong>: Introduzindo a Gestão!!! Áreas Operacionais e Gerencias</li>
-                <li>Aula <strong>M02.02</strong>: Arrumando sua Casa - Não espere ser roubado para trocar a fechadura!</li>
-                <li>Aula <strong>M02.03</strong>: Mapeie os processos e crie procedimentos (Entrada / Fornecedores / Processos / Saída / Cliente)</li>
-                <li>Aula <strong>M02.04</strong>: EAP – Estrutura Analítica de Projeto</li>
-                <li>Aula <strong>M02.05</strong>: MEN - Mínimo Esforço Necessário</li>
-            </ul>
-        </div>
-        <div class="col-img">
-            <picture>
-                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-mobile.png">
-                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-completo.png" alt="Ogro da bagunça">
-            </picture>  
-                <!-- <img class="bg" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-mobile.png" alt="">
-                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ogro-completo.png" alt=""> -->
-        </div>
+    <div class="start">
+        <a href="" class="btn-start">Press Start</a>
     </div>
-</section>
-<section class="modulo-03">
-    <div class="content-modulo">
-        <div class="col-info">
-            <h4>Boss 03</h4>
-            <h5>Princesa da Incerteza</h5>
-            <h3>MÓDULO 03: CAPTANDO, CONVERTENDO E PRECIFICANDO - 17/08/2023</h3>
-            <ul class="list-untyled">
-                <li>Aula <strong>M03.01</strong>: Básico para Captação de clientes On e Offline + Funil de vendas </li>
-                <li>Aula <strong>M03.02</strong>: BRANDING (Leticia Milani) + Material de Apoio</li>
-                <li>Aula <strong>M03.03</strong>: Básico para Conversão das propspecções geradas! Como converter</li>
-                <li>Aula <strong>M03.04</strong>: Básico para Precificação por HP (Não use m<strong>2</strong>)</li>
-                <li>Aula <strong>M03.05</strong>: Apresentando a proposta</li>
-            </ul>
-        </div>
-        <div class="col-img">
-            <picture>
-                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/princesa-incerteza-mobile.png">
-                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/princesa-incerteza.png" alt="Princesa da Incerteza">
-            </picture>  
-        </div>
-    </div>
-</section>
-<section class="modulo-04">
-    <div class="content-modulo">
-        <div class="col-img">
-            <picture>
-                <source media="(max-width: 600px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo-mobile.png">
-                <img class="boss" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sr-prejuizo.png" alt="SR. Prejuízo">
-            </picture>  
-        </div>
-        <div class="col-info">
-            <h4>Boss 04</h4>
-            <h5>SR. Prejuízo</h5>
-            <h3>MÓDULO 04: PILARES DE GESTÃO – 24/08/2023</h3>
-            <ul class="list-untyled">
-                <li>Aula <strong>M04.01</strong>: Básico para <strong>4</strong> pilares de gestão: Custos</li>
-                <li>Aula <strong>M04.02</strong>: Básico para <strong>4</strong> pilares de gestão: Prazos</li>
-                <li>Aula <strong>M04.03</strong>: Básico para <strong>4</strong> pilares de gestão: Pessoas</li>
-                <li>Aula <strong>M04.04</strong>: Básico para <strong>4</strong> pilares de gestão: Processos</li>
-            </ul>
-        </div>
-    </div>
+    <img class="detalhe2" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/datalhe-ogro01.png" alt="">
 </section>
 
+<script>
+       $('.desktop').owlCarousel({
+        loop:true,
+        nav:true,
+        dots:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            700:{
+                items:2
+            },
+            1000:{
+                items:2
+            }
+        }
+    })
+
+</script>
 
 <?php get_footer()?>
